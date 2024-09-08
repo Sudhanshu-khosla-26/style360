22a5209181b5cd6d009aee6452fbcec53be6439e
sudhanshu khosla <sudhanshukhosla123@gmail.com>
2024-09-09 00:00:22 +0530

    Init the project

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+  return (
+    <>
+       <h1 className="text-3xl bg-slate-300  w-full text-center font-bold">
+          This is navbar 
+          
+        </h1>
+       <h1 className="text-3xl bg-slate-300  w-full text-center font-bold">
+          Hello pandas!
+        </h1>
+    </>
+  )
+}
+
+export default Navbar;
